34abb9fb4dfb4e5bc60483e2ddbf996898e25d45
romeucajamba <romeucajamba@gmail.com>
2025-11-17 00:03:15 +0100

    created the projets and the first folder

---
+++ src/main/java/com/profesa/selos/SelosApplication.java
@@ -0,0 +1,13 @@
+package com.profesa.selos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/profesa/selos/SelosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profesa.selos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
