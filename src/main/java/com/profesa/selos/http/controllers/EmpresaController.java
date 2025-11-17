068d9e78f4ed42e23949272771ef72d3b666eaa8
romeucajamba <romeucajamba@gmail.com>
2025-11-17 03:46:33 +0100

    feat: Created controllers

--- src/main/java/com/profesa/selos/http/controllers/EmpresaController.java
+++ src/main/java/com/profesa/selos/http/controllers/EmpresaController.java
@@ -1,4 +1,30 @@
 package com.profesa.selos.http.controllers;
+import com.profesa.selos.domain.entities.Empresa;
+import com.profesa.selos.domain.entities.SelosFiscais;
+import com.profesa.selos.services.EmpresaService;
+import com.profesa.selos.services.SelosFiscaisService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/empresa")
+@RequiredArgsConstructor
 public class EmpresaController {
+    private EmpresaService service;
+
+    @PostMapping("/criar")
+    public ResponseEntity<Empresa> criarEmpresa(@RequestBody EmpresaDto  data){
+        Empresa interprise = service.criar(data);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(interprise);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Empresa> buscarEmpresaPodId(@PathVariable UUID id){
+        return ResponseEntity.of(service.findById(id));
+    }
 }
--- src/main/java/com/profesa/selos/http/controllers/SeloFiscaisController.java
+++ src/main/java/com/profesa/selos/http/controllers/SeloFiscaisController.java
@@ -1,4 +1,30 @@
 package com.profesa.selos.http.controllers;
 
+import com.profesa.selos.domain.entities.SelosFiscais;
+import com.profesa.selos.services.SelosFiscaisService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/selos")
+@RequiredArgsConstructor
 public class SeloFiscaisController {
+    private SelosFiscaisService service;
+
+    @PostMapping("/solicitar")
+    public ResponseEntity<SelosFiscais> solicitar(@RequestBody SeloRequestDto data, @RequestHeader("X-User") String user){
+        SelosFiscais selo = service.solicitarSelo(data.getEmpresaId(), data.getProduto(), user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(selo);
+    }
+
+    @PostMapping("/validar/{codigo}")
+    public ResponseEntity<Void> validar(@PathVariable String codigo, @RequestHeader("X-User") String user){
+        service.validarSelo(codigo, user);
+        return ResponseEntity.ok().build();
+    }
 }
