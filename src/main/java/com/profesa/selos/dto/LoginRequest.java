ca746e7863a9b5db6f5f8bfbc78251cd32af2622
romeucajamba <romeucajamba@gmail.com>
2025-11-26 17:57:05 +0100

    ajustes nos dtos de cadastro e login do usuario

--- src/main/java/com/profesa/selos/dto/LoginRequest.java
+++ src/main/java/com/profesa/selos/dto/LoginRequest.java
@@ -1,9 +1,17 @@
 package com.profesa.selos.dto;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Valid
 public record LoginRequest(
+        @Email(message = "Email inválido")
+        @NotBlank(message = "Email é obrigatório")
         String email,
+
+        @NotBlank(message = "Senha é obrigatória")
+        @Size(min = 6, message = "A senha deve ter pelo menos 6 caracteres")
         String senha
 ) {
 }
--- src/main/java/com/profesa/selos/dto/RegisterRequest.java
+++ src/main/java/com/profesa/selos/dto/RegisterRequest.java
@@ -1,9 +1,20 @@
 package com.profesa.selos.dto;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Valid
 public record RegisterRequest(
+        @Email(message = "Email inválido")
+        @NotBlank(message = "Email é obrigatório")
         String email,
-        String senha
+
+        @NotBlank(message = "Senha é obrigatória")
+        @Size(min = 6, message = "A senha deve ter pelo menos 6 caracteres")
+        String senha,
+
+        @NotBlank(message = "Confirmar senha é obrigatório")
+        String confirmarSenha
 ) {
 }
